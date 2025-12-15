4afa9fa31f5d4ff96c95cabf30df0fca86bc3661
suywh0 <suy.nepal@gmail.com>
2025-12-15 23:25:02 +0545

    feat: database initialization, posts table creation

--- app.go
+++ app.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/suynep/compilare/tests"
+import (
+	"github.com/suynep/compilare/database"
+	"github.com/suynep/compilare/tests"
+)
 
 func main() {
+	database.MustInitDB() // should be the first call. Always! :)
 	tests.TestJsonFetchTopStories()
 }
