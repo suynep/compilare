8b2902b1844289eca9c7da0fe6b4f31c213c3689
suywh0 <suy.nepal@gmail.com>
2025-12-30 11:03:23 +0545

    feat: add sha256 hashes for saving p/w in db

--- crypt/gen.go
+++ crypt/gen.go
@@ -1,10 +1,20 @@
 package crypt
 
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
 func GenerateSessionKey(salt string) {
 
 }
 
 func HashPassword(password string) string {
 	// placeholder for the time being; actual logic shall be implemented soon
-	return password
+	h := sha256.New()
+	h.Write([]byte(password))
+
+	bs := h.Sum(nil)
+
+	return fmt.Sprintf("%x", bs)
 }
